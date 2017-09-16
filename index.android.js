d4d7c2536db17d968db95ef032285ba3d1da01b9
liuyandong <554030404@qq.com>
2017-09-16 22:22:33 +0800

    初始化项目！！！

---
+++ index.android.js
@@ -0,0 +1,8 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import {AppRegistry} from "react-native";
+import MainComponent from "./components/MainComponent";
+AppRegistry.registerComponent("MainComponent", () => MainComponent);
