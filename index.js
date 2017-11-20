f0b827cdb9f12b1c97607475f73cc6ec0b90ac0c
liuyandong <554030404@qq.com>
2017-11-20 17:02:24 +0800

    支付宝支付！！

--- index.js
+++ index.js
@@ -2,27 +2,65 @@
  * Created by liuyandong on 2017/11/17.
  */
 import React, {Component} from "react";
-import {AppRegistry, Navigator} from "react-native";
-// import {Navigator} from "react-native-deprecated-custom-components";
+import {
+    AppRegistry,
+    View,
+    Text,
+    Image,
+    Dimensions,
+    StatusBar,
+    FlatList,
+    PixelRatio
+} from "react-native";
 import LoginView from "./views/login/LoginView";
+import Swiper from "react-native-swiper";
 
-import NavigatorView from "./views/navigator/NavigatorView";
-import HH from "./views/login/LoginView";
+const window = Dimensions.get("window");
+const width = window.width;
+const height = window.height;
+const statusBarHeight = StatusBar.currentHeight;
+const pixelWidth = 1 / PixelRatio.get();
+class MainComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: [{id: 100, name: "刘艳东"}, {id: 200, name: "liuyandong"}]};
+    }
 
-/*class MainComponent extends Component {
-    render() {
+    renderItem(item, index, separators) {
         return (
-            <Navigator
-                initialRoute={{component: LoginView, parameters: {}}}
-                renderScene={(route, navigator) =>
-                    <route.component route={route} navigator={navigator} {...route.parameters}></route.component>
-                }
-                configureScene={(route) => {
-                    return Navigator.SceneConfigs.FloatFromRight
-                }}>
-            </Navigator>
+            <View style={{borderBottomWidth: pixelWidth, marginLeft: 10}}>
+                <Text key={index}>{item["item"]["name"]}</Text>
+            </View>
         );
     }
-}*/
 
-AppRegistry.registerComponent("MainComponent", () => LoginView);
+    keyExtractor(item, index) {
+        return item.id;
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Swiper loop={true} autoplay={true} style={{height: 200}}>
+                    <View style={{height: 150}}>
+                        <Image resizeMode="stretch" style={{flex: 1}} source={{uri: "http://ubmcmm.baidustatic.com/media/v1/0f0005DL5ZsPBJreEWzsa0.jpg"}}></Image>
+                    </View>
+                    <View style={{height: 150, backgroundColor: "yellow"}}>
+                        <Image resizeMode="stretch" style={{flex: 1}} source={{uri: "http://ubmcmm.baidustatic.com/media/v1/0f000ckyT6Y8ZgUHgku-06.jpg"}}></Image>
+                    </View>
+                    <View style={{height: 150, backgroundColor: "green"}}>
+                        <Image resizeMode="stretch" style={{flex: 1}} source={{uri: "http://image.beekka.com/blog/2015/bg2015031302.jpg"}}></Image>
+                    </View>
+                </Swiper>
+                <View style={{height: height - 150 - statusBarHeight}}>
+                    <FlatList data={this.state.data}
+                              keyExtractor={this.keyExtractor}
+                              renderItem={this.renderItem}>
+                    </FlatList>
+                </View>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent("MainComponent", () => MainComponent);
